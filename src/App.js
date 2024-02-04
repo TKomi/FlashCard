9a6adcded4fe686af195a61778bb1339e4336ff3
TKomi <37932914+TKomi@users.noreply.github.com>
2024-02-04 17:52:09 +0900

    TODO-19: 回答履歴の保存 (#7)
    * クイズ終了時に結果を保存する
    
    ステータスの更新処理を追加
    
    * ステータス更新処理を修正
    
    * 学習セッションでの正誤判定結果を取り出して使い回せるようにした
    
    * 単語の学習ステータスをリザルト画面に表示する

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import StudyScreen from './StudyScreen/StudyScreen';
 import ResultScreen from './ResultScreen';
 import { Word } from './models/Word';
 import { createQuiz4 } from './StudyScreen/CreateQuiz';
+import { LearningSession } from './models/LearningSession';
+import { v4 as uuidv4 } from 'uuid';
+import { WordStatus } from './models/WordStatus';
+import { LS } from './store/LS';
 
 function App() {
   const [currentScreen, setCurrentScreen] = useState('study');
@@ -17,6 +21,9 @@ function App() {
 
   // ユーザーの回答の一覧
   const [userAnswers, setUserAnswers] = useState([]);
+
+  // クイズの一覧の順番に対応する、単語の学習状況の一覧(更新後)
+  const [wordStatus, setWordStatus] = useState([]);
 
   // 起動時処理
   useEffect(() => {
@@ -32,17 +39,29 @@ function App() {
       .catch(console.error);
   }, []);
 
+  // クイズ終了時処理
+  const onEndQuiz = (ua) => {
+    // wordList, quizzes, userAnswersの内容を追記
+    const storageData = {
+      learningSession: [],
+      wordStatus: {},
+    }; // 仮実装
+
+    setUserAnswers(ua);
+    const updatedWordsStatuses = updateWordStatuses(wordList, quizzes, ua, storageData);
+    setWordStatus(updatedWordsStatuses);
+    save(wordList, quizzes, ua, storageData, updatedWordsStatuses);
+    setCurrentScreen('result');
+  };
+
   return (
     <div>
       <h1>TOEIC単語帳</h1>
       {
         currentScreen === 'study' ? (
-          <StudyScreen quizzes={quizzes} onEndQuiz={(userAnswers) => {
-            setUserAnswers(userAnswers);
-            setCurrentScreen('result');
-          }} />
+          <StudyScreen quizzes={quizzes} onEndQuiz={onEndQuiz} />
         ) : currentScreen === 'result' ? (
-          <ResultScreen words={wordList} quizzes={quizzes} userAnswers={userAnswers} />
+          <ResultScreen words={wordList} quizzes={quizzes} userAnswers={userAnswers} wordStatus={wordStatus} />
         ) : (
           <div>
             <p>エラー</p>
@@ -54,8 +73,124 @@ function App() {
 }
 
 /**
+ * 学習セッション1件の情報を保存(追記)する
+ * 
+ * @param {Word[]} wordList 出題された単語の一覧
+ * @param {Quiz[]} quizzes 出題された問題の一覧。wordListと順番が同じである
+ * @param {number[]} userAnswers ユーザーの回答の一覧。quizzesと順番が同じである
+ * @param {import("./models/LearningSession").LearningSession} oldLearningSession LSから読み込んだ、既存の学習セッションデータ
+ * - delete-insert方式のため、古いデータも必要となる
+ * @param {WordStatus[]} updatedWordsStatuses 更新後の単語の学習状況の一覧
+ * - updateWordStatusesで作成したものをわたすこと
+ * - updateWordStatuses関数の結果は他の箇所でも使いたかったため関数は分離した
+ */
+function save(wordList, quizzes, userAnswers, oldLearningSession, updatedWordsStatuses) {
+// 学習セッションの状況
+  const learningSessionClone = [...oldLearningSession.learningSession];
+  learningSessionClone.push(new LearningSession({
+    sessionId: uuidv4(),
+    wordSetId: 'sample', // 仮実装
+    completionDate: new Date().toISOString(),
+    answerHistory: userAnswers.map((answer, index) => {
+      return {
+        w: wordList[index].word,
+        c: quizzes[index].answerIndex === answer,
+      };
+    }),
+  }));
+
+  // updatedWordStatusesを詰め直す
+  const wordStatusClone = { ...oldLearningSession.wordStatus };
+  for(const st of updatedWordsStatuses){
+    wordStatusClone[st.word] = st;
+  }
+
+  // LocalStorageに保存する
+  LS.save({
+    learningSession: learningSessionClone,
+    wordStatus: wordStatusClone,
+  });
+}
+
+/**
+ * 学習セッション1件の終了後の各単語の学習状況(単語ステータス)を取得する
+ * 
+ * @param {Word[]} wordList 単語の一覧
+ * @param {Quiz[]} quizzes クイズの一覧
+ * - 順番は単語の一覧と同じ
+ * @param {number[]} userAnswers ユーザーの回答の一覧
+ * - 順番は単語の一覧と同じ
+ * @param {import("./store/LS").FlashCardData} saveData LSから読み込んだ、既存のデータ
+ * @returns {WordStatus[]} 単語の学習状況の一覧
+ * - 単語(クイズ, 回答)の一覧と同じ順番で、各単語の学習状況を格納した配列
+ */
+function updateWordStatuses(wordList, quizzes, userAnswers, saveData) {
+  const result = [];
+
+  for (const word of wordList) {
+    // 対応するwordStatusを取得, なければ新規作成
+    const updated = saveData.wordStatus[word.word] || new WordStatus(word.word, new Date().toISOString(), [], 0);
+
+    // dateを更新
+    updated.lastStudied = new Date().toISOString();
+
+    // answerHistoryに最後の問題の正誤を追加
+    const quizCorrectness = getQuizCorrectness(word.word, quizzes, userAnswers);
+    updated.answerHistory.push(quizCorrectness);
+
+    // statusを更新
+    updated.status = getUpdatedStatus(updated.status, quizCorrectness);
+
+    // wordStatusを更新
+    result.push(updated);
+  }
+
+  return result;
+}
+
+/** クイズの一覧およびユーザーの回答の一覧を提供し、指定した単語に関するクイズ1問の正誤を取得する
+ * 
+ * @param {string} word 単語
+ * @param {Quiz[]} quizzes クイズの一覧
+ * @param {number[]} userAnswers ユーザーの回答の一覧
+ * @returns {boolean} 正解ならtrue, 不正解ならfalse
+ */
+function getQuizCorrectness(word, quizzes, userAnswers) {
+  const index = quizzes.findIndex(quiz => quiz.question === word);
+  if (index !== -1) {
+    return quizzes[index].answerIndex === userAnswers[index];
+  }
+  return false;
+}
+
+/**
+ * 単語の現在の状態(未回答0、苦手1、うろ覚え2、覚えた3), 今回の回答が正解かどうか, チェックボックスの状態から、更新後のステータスを返す
+ * 
+ * @param {number} oldStatus 現在のステータス
+ * @param {boolean} ansIsCorrect 今回の回答が正解かどうか
+ * @param {boolean} checked チェックボックスの状態 // 未実装
+ * @returns {0|1|2|3} 更新後のステータス
+ */
+function getUpdatedStatus(oldStatus, ansIsCorrect, checked = false){
+  // 現在の状態はcheckedが反映されていない仮のもの
+  switch(oldStatus) {
+    case 0:
+      return ansIsCorrect ? 3 : 2;
+    case 1:
+      return ansIsCorrect ? 2 : 1;
+    case 2:
+      return ansIsCorrect ? 3 : 1;
+    case 3:
+      return ansIsCorrect ? 3 : 2;
+    default:
+      return 0;
+  }
+}
+
+
+/**
  * 配列をシャッフルする
- * @param {*} array シャッフル対象の配列 副作用はありません。
+ * @param {any[]} array シャッフル対象の配列 副作用はありません。
  * @returns シャッフルされた配列
  */
 function getShuffledArray(array) {
--- src/ResultScreen.js
+++ src/ResultScreen.js
@@ -13,9 +13,9 @@ import React, { useEffect, useState } from 'react';
  * - ユーザーの回答の記録
  * - 単語、問題のリスト、ユーザーの回答リストは受け取るだけで、自分で作成しない
  * 
- * @param {{words: Word[], quizzes: Quiz[], userAnswers: number[]}} props 画面で扱う単語リスト、クイズの一覧、ユーザーの回答
+ * @param {{words: Word[], quizzes: Quiz[], userAnswers: number[], wordStatus: import("./models/WordStatus").WordStatus}} props 画面で扱う単語リスト、クイズの一覧、ユーザーの回答、各単語の学習状況
  */
-function ResultScreen({ words, quizzes, userAnswers }) {
+function ResultScreen({ words, quizzes, userAnswers, wordStatus }) {
 
   const [entries, setEntries] = useState([]);
 
@@ -25,10 +25,11 @@ function ResultScreen({ words, quizzes, userAnswers }) {
       const spelling = word.word;
       const correctAnswer = word.quiz.answer; // string
       const isCorrect = quiz.answerIndex === userAnswers[index];
-      return { index, spelling, correctAnswer, isCorrect };
+      const status = wordStatus[index].status;
+      return { index, spelling, correctAnswer, isCorrect, status };
     });
     setEntries(newEntries);
-  }, [words, quizzes, userAnswers]);
+  }, [words, quizzes, userAnswers, wordStatus]);
 
   return (
     <div>
@@ -39,11 +40,31 @@ function ResultScreen({ words, quizzes, userAnswers }) {
             <div className="result-spelling">{entry.spelling}</div>
             <div className="result-answer">{entry.correctAnswer}</div>
             <div className="result-isCorrect">{entry.isCorrect ? '○' : '×'}</div>
+            <ResultStatus wordStatus={entry.status} />
           </li>
         ))}
       </ul>
     </div>
   );
+}
+
+/**
+ * 「覚えた」「うろ覚え」などの学習状況を表示するコンポーネント
+ * @param {{wordStatus: 0|1|2|3}}
+ * @returns JSX.Element
+ */
+function ResultStatus({ wordStatus }) {
+  switch(wordStatus) {
+    case 3:
+      return (<div className="result-status-3">覚えた</div>);
+    case 2:
+      return (<div className="result-status-2">うろ覚え</div>);
+    case 1:
+      return (<div className="result-status-1">苦手</div>);
+    case 0:
+    default:
+      return (<div className="result-status-0">未学習</div>);
+  }
 }
 
 export default ResultScreen;
