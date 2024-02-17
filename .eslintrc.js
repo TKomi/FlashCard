27abd704f640dedd70a7bb6ff1bd5629485f1d3f
TKomi <t.komi2580@gmail.com>
2024-02-17 13:48:42 +0900

    Linterを設定

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    'no-case-declarations': 'off',
+  }
+};
