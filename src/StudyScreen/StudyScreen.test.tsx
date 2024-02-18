bc0e6039f3f9a4e3068e6601b46f5404b5d45ac6
TKomi <t.komi2580@gmail.com>
2024-02-18 17:08:17 +0900

    「問題の表示と回答 基本的な流れ」のテストをリファクタリング、加筆

--- src/StudyScreen/StudyScreen.test.tsx
+++ src/StudyScreen/StudyScreen.test.tsx
@@ -7,9 +7,9 @@ jest.useFakeTimers();
 
 describe('StudyScreen', () => {
   const quizzes = [
-    new Quiz('Question 1', 0, ['Option 1', 'Option 2', 'Option 3', 'Option 4']),
-    new Quiz('Question 2', 1, ['Option 1', 'Option 2', 'Option 3', 'Option 4']),
-    new Quiz('Question 3', 2, ['Option 1', 'Option 2', 'Option 3', 'Option 4']),
+    new Quiz('Question 1', 0, ['Option 10', 'Option 11', 'Option 12', 'Option 13']),
+    new Quiz('Question 2', 1, ['Option 20', 'Option 21', 'Option 22', 'Option 23']),
+    new Quiz('Question 3', 2, ['Option 30', 'Option 31', 'Option 32', 'Option 33']),
   ];
 
   const onEndQuiz = jest.fn();
@@ -28,86 +28,45 @@ describe('StudyScreen', () => {
     expect(screen.getByText('Question 1')).toBeInTheDocument();
   });
 
-  it('問題の表示と回答 基本的な流れ', async () => {
+  it('問題の表示と回答 基本的な流れ 正常終了', async () => {
     // 1問目
     act(() => {
-      fireEvent.click(screen.getByText('Option 1'));
+      fireEvent.click(screen.getByText('Option 11'));
+      jest.advanceTimersByTime(1500);
     });
+
+    // 2問目 画面遷移とスタイルの確認 正答時
     await waitFor(() => {
       expect(screen.getByText('Question 2')).toBeInTheDocument();
-    }, { timeout: 1500 });
-
-    // 2問目
-    act(() => {
-      setTimeout(() => {
-        act(() => {
-          fireEvent.click(screen.getByText('Option 2'));
-        });
-      }, 1500);
-      jest.advanceTimersByTime(1500); // タイムライン: 1500ms
-    });
+      act(() => {
+        fireEvent.click(screen.getByText('Option 21'));
+        jest.advanceTimersByTime(1500);
+      });
+    }, { timeout: 3000 });
+    
+    // 3問目 画面遷移とスタイルの確認 誤答時
+    // 2問目のチェックがクリアされる
     await waitFor(() => {
       expect(screen.getByText('Question 3')).toBeInTheDocument();
-    }, { timeout: 1500 });
-    
-    // 3問目
-    act(() => {
-      setTimeout(() => {
-        act(() => {
-          fireEvent.click(screen.getByText('Option 3'));
-        });
-      }, 3000);
-      jest.advanceTimersByTime(1500); // タイムライン: 3000ms
-    });
+      act(() => {
+        fireEvent.click(screen.getByText('Option 31'));
+        jest.advanceTimersByTime(1500);
+      });
+    }, { timeout: 3000 });
+
+    // 4問目 画面遷移とスタイルの確認 スキップ時
+  
+    // 結果発表
     await waitFor(() => {
       expect(onEndQuiz).toHaveBeenCalledTimes(1);
       expect(onEndQuiz).toHaveBeenCalledWith([
-        { option: 0, checked: false },
         { option: 1, checked: false },
-        { option: 2, checked: false },
+        { option: 1, checked: false },
+        { option: 1, checked: false },
       ]);
-    }, { timeout: 6000 });
+    }, { timeout: 3000 });
   });
 
-  it.skip('次の問題に進むこと', () => {
-    fireEvent.click(screen.getByText('Option 1'));
-    fireEvent.click(screen.getByText('Option 2'));
-    fireEvent.click(screen.getByText('Option 3'));
-    fireEvent.click(screen.getByText('Option 4'));
-
-    expect(screen.getByText('Question 2')).toBeInTheDocument();
-  });
-
-  it.skip('クイズが終了した時にonEndQuizが呼ばれること', () => {
-    fireEvent.click(screen.getByText('Option 1'));
-    fireEvent.click(screen.getByText('Option 2'));
-    fireEvent.click(screen.getByText('Option 3'));
-    fireEvent.click(screen.getByText('Option 4'));
-
-    fireEvent.click(screen.getByText('Option 1'));
-    fireEvent.click(screen.getByText('Option 2'));
-    fireEvent.click(screen.getByText('Option 3'));
-    fireEvent.click(screen.getByText('Option 4'));
-
-    fireEvent.click(screen.getByText('Option 1'));
-    fireEvent.click(screen.getByText('Option 2'));
-    fireEvent.click(screen.getByText('Option 3'));
-    fireEvent.click(screen.getByText('Option 4'));
-
-    expect(onEndQuiz).toHaveBeenCalledTimes(1);
-    expect(onEndQuiz).toHaveBeenCalledWith([
-      { option: 0, checked: false },
-      { option: 1, checked: false },
-      { option: 2, checked: false },
-      { option: 3, checked: false },
-      { option: 0, checked: false },
-      { option: 1, checked: false },
-      { option: 2, checked: false },
-      { option: 3, checked: false },
-      { option: 0, checked: false },
-      { option: 1, checked: false },
-      { option: 2, checked: false },
-      { option: 3, checked: false },
-    ]);
-  });
+  test.todo('基本的な流れ 「やめる」選択時 1問以上回答済み');
+  test.todo('基本的な流れ 「やめる」選択時 1問も解いていない');
 });
