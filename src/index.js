786e2648197cdf4e2c17bee4fe53e982039da078
TKomi <37932914+TKomi@users.noreply.github.com>
2024-03-02 16:15:07 +0900

    TODO-57: Googleアナリティクス関連コード (#35)
    * Google Analytics 関連コード
    
    * .envのサンプルを配置

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+// log
 if(process.env.REACT_APP_COMMIT_SHA) {
   console.info('アプリケーション コミットID: ', process.env.REACT_APP_COMMIT_SHA);
 } else {
   console.warn('環境変数`REACT_APP_COMMIT_SHA`を読み込めませんでした。 .env ファイルを配置し忘れましたか？');
 }
+
+// Google Analytics
+const googleAnalyticsId = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
+document.write(`<script async src="https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}"></script>`);
+window.dataLayer = window.dataLayer || [];
+function gtag() { window.dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', googleAnalyticsId);
+
+// render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
